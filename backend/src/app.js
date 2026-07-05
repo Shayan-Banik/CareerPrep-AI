e3531ce1e8525a89e5d845265a4b0c6b95724f9b
Shayan Banik <onlyforlearning100@gmail.com>
2026-07-05 21:32:14 +0530

    add frontend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'career-prep-ai-sigma.vercel.app',
     credentials: true
 }));
 
